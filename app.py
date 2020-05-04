c7625b44e19d484d04619b5be08b9af0a41cd4f5
iago <iagoc.dev@gmail.com>
2020-05-04 15:09:22 -0300

    add schemas

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
+from flask import Flask, jsonify
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/database.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATORS'] = False
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 migrate = Migrate(app, db)
 
 
@@ -31,13 +33,29 @@ class File(db.Model):
     def __init__(self, id, name, directory, user_id):
         self.id = id
         self.name = name
-        self.directory = email
+        self.directory = directory
         self.user_id = user_id
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email', 'password')
+
+class FileSchema(ma.Schema):
+    class Meta:
+        fields = ('name', 'directory', 'user_id')
+
+
+user_schema = UserSchema()
+users_schemas = UserSchema(many=True)
+
+file_schema = FileSchema()
+files_schemas = FileSchema(many=True)
 
 @app.route('/')
 def hello():
-    # code
-    return "Hello World!"
+    all_users = User.query.all()
+    result = users_schemas.dump(all_users)
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run()
