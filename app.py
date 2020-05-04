bab2846c5f38e836a63cd82db59414a9e8ce916a
wilson <wilson.sfj@outlook.com>
2020-05-04 17:51:19 -0300

    [2] download e upload de arquivos

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, send_file
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+from io import BytesIO
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/database.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATORS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
@@ -26,14 +27,13 @@ class User(db.Model):
 
 class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200))
-    directory = db.Column(db.String(200))
+    name = db.Column(db.String(255))
+    data = db.Column(db.String(255))
     user_id = db.Column(db.Integer())
 
-    def __init__(self, id, name, directory, user_id):
-        self.id = id
+    def __init__(self, name, data, user_id):
         self.name = name
-        self.directory = directory
+        self.data = data
         self.user_id = user_id
 
 class UserSchema(ma.Schema):
@@ -56,6 +56,30 @@ def hello():
     all_users = User.query.all()
     result = users_schemas.dump(all_users)
     return jsonify(result)
+
+@app.route('/files', methods=['GET'])
+def showFiles():
+    all_files = File.query.all()
+    result = files_schemas.dump(all_files)
+
+    return jsonify({'files': result})
+
+@app.route('/files', methods=['POST'])
+def uploadFiles():
+
+    file_request = request.files['file']
+
+    new_file = File(file_request.filename, file_request.read(), 1)
+    db.session.add(new_file)
+    db.session.commit()
+
+    return jsonify({'success': True})
+
+@app.route('/files/download')
+def downloadFiles():
+    file_data = File.query.filter_by(id=1).first()
+
+    return send_file(BytesIO(file_data.data), attachment_filename='flask.pdf', as_attachment=True)
 
 if __name__ == '__main__':
     app.run()
