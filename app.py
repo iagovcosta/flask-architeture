e91e26f44e6ab729d065e75d4f40222d5adedca8
wilson <wilson.sfj@outlook.com>
2020-05-04 21:39:25 -0300

    [#2] implementando softDelete

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, send_file
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, BaseQuery
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from werkzeug.utils import secure_filename
@@ -19,6 +19,33 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
 
+class QueryWithSoftDelete(BaseQuery):
+    _with_deleted = False
+
+    def __new__(cls, *args, **kwargs):
+        obj = super(QueryWithSoftDelete, cls).__new__(cls)
+        obj._with_deleted = kwargs.pop('_with_deleted', False)
+        if len(args) > 0:
+            super(QueryWithSoftDelete, obj).__init__(*args, **kwargs)
+            return obj.filter_by(deleted=False) if not obj._with_deleted else obj
+        return obj
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def with_deleted(self):
+        return self.__class__(db.class_mapper(self._mapper_zero().class_),
+                              session=db.session(), _with_deleted=True)
+
+    def _get(self, *args, **kwargs):
+        # this calls the original query.get function from the base class
+        return super(QueryWithSoftDelete, self).get(*args, **kwargs)
+
+    def get(self, *args, **kwargs):
+        # the query.get method does not like it if there is a filter clause
+        # pre-loaded, so we need to implement it using a workaround
+        obj = self.with_deleted()._get(*args, **kwargs)
+        return obj if obj is None or self._with_deleted or not obj.deleted else None
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -39,11 +66,20 @@ class File(db.Model):
     user_id = db.Column(db.Integer())
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow, server_onupdate=db.func.now())
+    deleted = db.Column(db.Boolean(), default=False)
+
+    query_class = QueryWithSoftDelete
 
     def __init__(self, name, path_file, user_id):
         self.name = name
         self.path_file = path_file
         self.user_id = user_id
+
+    def to_dict(self):
+        return {'id': self.id, 'message': self.message,
+                'url': url_for('get_message', id=self.id),
+                'user_url': url_for('get_user', id=self.user_id)
+                if not self.user.deleted else None}
 
 class UserSchema(ma.Schema):
     class Meta:
@@ -97,3 +133,10 @@ def uploadFiles():
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], hased_name))
 
         return jsonify({'success': True, 'message': 'Arquivo salvo com sucesso'})
+
+@app.route('/files/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    file = File.query.get_or_404(id)
+    file.deleted = True
+    db.session.commit()
+    return '', 204
