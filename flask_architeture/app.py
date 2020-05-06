33c3e4dd4485657844234eca85a9098fa73940c9
wilson <wilson.sfj@outlook.com>
2020-05-06 09:44:35 -0300

    [#28] Organizando rotas da web

--- flask_architeture/app.py
+++ flask_architeture/app.py
@@ -3,7 +3,7 @@ from flask_architeture.extensions import configuration
 from flask_architeture.extensions.database import db, init_app
 from flask_architeture.extensions import migrations
 from flask_architeture.extensions import bootstrap
-from flask_architeture.bussines.web import views
+from flask_architeture.bussines import web
 from flask_architeture.bussines import resources
  
 
@@ -15,4 +15,5 @@ init_app(app) #database_factory
 migrations.init_app(app, db)
 resources.init_app(app)
 bootstrap.init_app(app)
-views.init_app(app, db)
+web.init_app(app)
+# views.init_app(app, db)
