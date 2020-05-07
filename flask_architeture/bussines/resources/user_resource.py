f926ed62c82f6989f9ed3b4be03b536ebdba86fc
iago <iagoc.dev@gmail.com>
2020-05-06 21:42:29 -0300

    [#19] Ajustes nas rotas web dos usuarios

--- flask_architeture/bussines/resources/user_resource.py
+++ flask_architeture/bussines/resources/user_resource.py
@@ -6,11 +6,6 @@ from .token_decorator import token_required
 import string
 import random
 
-# random_str = string.ascii_letters + string.digits + string.ascii_uppercase
-# key = ''.join(random.choice(random_str) for i in range(12))
-# SECRET_KEY = key
-
-
 
 class UserResource(Resource):
 
--- flask_architeture/bussines/web/__init__.py
+++ flask_architeture/bussines/web/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 from .files_web import show_files, upload_file, delete_file, render_upload
+from .users_web import render_register, create_user, render_update, render_login
+# from flask_architeture.resources import au
 
 bp = Blueprint("web", __name__, template_folder="templates")
 
@@ -10,6 +12,10 @@ bp.add_url_rule("/files/upload", view_func=render_upload)
 bp.add_url_rule("/files", methods=['POST'], view_func=upload_file)
 
 #USERS
+bp.add_url_rule("/register", view_func=render_register)
+bp.add_url_rule("/user/register", view_func=create_user, methods=['POST'])
+bp.add_url_rule("/user/update", view_func=render_update)
+bp.add_url_rule("/login", view_func=render_login)
 
 def init_app(app):
     app.register_blueprint(bp)
